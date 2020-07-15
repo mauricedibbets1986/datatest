6af0c546cc7fc5c2612367715f95b7545bbe5892
mauricedibbets1986 <mauricedibbets1986@gmail.com>
2020-07-15 16:46:56 +0200

    latest commit 2

--- src/main/java/com/demo/rest/EmployeeEndpoint.java
+++ src/main/java/com/demo/rest/EmployeeEndpoint.java
@@ -1,15 +1,23 @@
 package com.demo.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.demo.controller.EmployeeService;
+
 @RestController
 public class EmployeeEndpoint {
-
+	@Autowired
+	EmployeeService es;
+	
 	@GetMapping("/abc")
+	
 	void jojo() {
 		System.out.println("endpoint aangeroepen");
+		es.opslaan();
 	}
+	
 	
 	
 }
